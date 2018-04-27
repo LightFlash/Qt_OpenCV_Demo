76f6438dbb6a8f320507ad209be9460abca9c146
LightFlash <fatescene520@gmail.com>
2018-04-28 00:32:39 +0800

    图像腐蚀程序

--- 0001-HelloOpenCV/main.cpp
+++ 0001-HelloOpenCV/main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
-
+#include <opencv2/opencv.hpp>
+using namespace cv;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    Mat img = imread("C:/Users/fates/Desktop/OpenCVImg/0001-1.jpg");
+    imshow("Picture", img);
     return a.exec();
 }
---
+++ 0002-ImageErode/main.cpp
@@ -0,0 +1,17 @@
+#include <QCoreApplication>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    Mat srcImg = imread("C:/Users/fates/Desktop/OpenCVImg/0002-1.jpg");
+    imshow("srcImg", srcImg);
+
+    //矩形腐蚀
+    Mat element = getStructuringElement(MORPH_RECT, Size(20, 20));
+    Mat dstImg;
+    erode(srcImg, dstImg, element);
+    imshow("dstImg", dstImg);
+    return a.exec();
+}
